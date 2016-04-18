3f682308a3feebeb78e8d447f466f216db58d494
songbaby <472711629@qq.com>
2016-04-18 08:53:26 +0800

    socket.cpp
    add socket.cpp

--- src/libev.cpp
+++ src/libev.cpp
@@ -9,7 +9,14 @@
 
 #include <ev.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <string>
+#include <fcntl.h>
 
+
+ev_io ev_io_accept;
 ev_io stdin_watcher;
 ev_timer timeout_watcher;
 
@@ -26,17 +33,133 @@ static void timeout_cb (EV_P_ ev_timer *w,int revents)
     ev_break( EV_A_ EVBREAK_ONE );
 }
 
-int main(int argc,char* argv[])
+
+
+
+
+int main1(int argc,char* argv[])
 {
     struct ev_loop *loop = EV_DEFAULT;
 
     ev_io_init(&stdin_watcher,stdin_cb,0,EV_READ);
     ev_io_start (loop,&stdin_watcher);
 
-    ev_timer_init (&timeout_watcher,timeout_cb,20.5,0); // in document the last is 0. not 0,I don't know why yet
+    ev_timer_init (&timeout_watcher,timeout_cb,2.5,0); // in document the last is 0. not 0,I don't know why yet
     ev_timer_start (loop,&timeout_watcher);
+
+
+
+
+
+
+
 
     ev_run (loop,0);
 
     return 0;
 }
+
+int set_reuseport(int socket)
+{
+    int opt = 1;
+    return setsockopt(socket, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt));
+}
+int setnonblocking(int fd)
+{
+    int flags;
+    if (-1 == (flags = fcntl(fd, F_GETFL, 0))) {
+        flags = 0;
+    }
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
+int create_and_bind(const char *host, const char *port)
+{
+    struct addrinfo hints;
+    struct addrinfo *result, *rp, *ipv4v6bindall;
+    int s, listen_sock;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family   = AF_UNSPEC;               /* Return IPv4 and IPv6 choices */
+    hints.ai_socktype = SOCK_STREAM;             /* We want a TCP socket */
+    hints.ai_flags    = AI_PASSIVE | AI_ADDRCONFIG; /* For wildcard IP address */
+    hints.ai_protocol = IPPROTO_TCP;
+
+    for (int i = 1; i < 8; i++) {
+        s = getaddrinfo(host, port, &hints, &result);
+        if (s == 0) {
+            break;
+        } else {
+           //sleep(pow(2, i));
+           // LOGE("failed to resolve server name, wait %.0f seconds", pow(2, i));
+        }
+    }
+
+    if (s != 0) {
+        //LOGE("getaddrinfo: %s", gai_strerror(s));
+        return -1;
+    }
+
+    rp = result;
+
+    /*
+     * On Linux, with net.ipv6.bindv6only = 0 (the default), getaddrinfo(NULL) with
+     * AI_PASSIVE returns 0.0.0.0 and :: (in this order). AI_PASSIVE was meant to
+     * return a list of addresses to listen on, but it is impossible to listen on
+     * 0.0.0.0 and :: at the same time, if :: implies dualstack mode.
+     */
+    if (!host) {
+        ipv4v6bindall = result;
+
+        /* Loop over all address infos found until a IPV6 address is found. */
+        while (ipv4v6bindall) {
+            if (ipv4v6bindall->ai_family == AF_INET6) {
+                rp = ipv4v6bindall; /* Take first IPV6 address available */
+                break;
+            }
+            ipv4v6bindall = ipv4v6bindall->ai_next; /* Get next address info, if any */
+        }
+    }
+
+    for (/*rp = result*/; rp != NULL; rp = rp->ai_next) {
+        listen_sock = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (listen_sock == -1) {
+            continue;
+        }
+
+        if (rp->ai_family == AF_INET6) {
+            int ipv6only = host ? 1 : 0;
+            setsockopt(listen_sock, IPPROTO_IPV6, IPV6_V6ONLY, &ipv6only, sizeof(ipv6only));
+        }
+
+        int opt = 1;
+        setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+#ifdef SO_NOSIGPIPE
+        setsockopt(listen_sock, SOL_SOCKET, SO_NOSIGPIPE, &opt, sizeof(opt));
+#endif
+        int err = set_reuseport(listen_sock);
+        if (err == 0) {
+           // LOGI("port reuse enabled");
+        }
+
+        s = bind(listen_sock, rp->ai_addr, rp->ai_addrlen);
+        if (s == 0) {
+            /* We managed to bind successfully! */
+            break;
+        } else {
+          //  ERROR("bind");
+        }
+
+        close(listen_sock);
+    }
+
+    if (rp == NULL) {
+        //LOGE("Could not bind");
+        return -1;
+    }
+
+    freeaddrinfo(result);
+
+    return listen_sock;
+}
+
